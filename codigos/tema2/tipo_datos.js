e1c6119af019e3f01045268c7f7f82ad1636f803
Ronny Gonzalez <yordemblo@gmail.com>
2023-11-12 15:58:49 +0100

    Añadido c´´odigo del tema 2, enlace de conferencia y cuestionario

---
+++ codigos/tema2/tipo_datos.js
@@ -0,0 +1,56 @@
+//Variable de tipo numérico
+let num1 = 10;
+console.log("El valor de esta variable es: " + typeof num1);
+
+
+//Variable con valor decimal
+let decimal = 5.5;
+console.log("El valor de esta variable decimal es: " + typeof decimal);
+
+let decimal2 = 1.2;
+
+//Variable de tipo string o cadena
+let cadena = "Hola mundo";
+console.log("El valor de la cadena es: " + typeof cadena);
+
+
+//Variable de tipo de dato booleano
+let mi_boolean = true;
+console.log("El valor de variable booleana es: " + typeof mi_boolean);
+
+
+//Ejemplo de contatenar con más(+)
+let valor1 = "5";
+let valor2 = 5;
+console.log("El resultado es: " + valor1 + valor2);
+
+//Longitud de una cadena
+let longitud = "hola mundo";
+console.log(longitud.length);
+
+//Imprimiendo el valor de una cadena en su índice
+console.log(longitud.indexOf("o"));
+
+//Variable no asignada
+let no_asignada;
+console.log("el valor es: " + typeof no_asignada);
+
+//Ejemplo de operadores
+let operador1 = 15;
+let operador2 = 10;
+//let resultado = operador1 += operador2;
+//console.log(resultado);
+
+//operador1 += 100;
+//console.log(operador1);
+
+operador1 = 100;
+console.log(operador1);
+
+//Decrementando
+operador1--;
+console.log(operador1)
+
+//Incrementando
+operador1++;
+console.log(operador1)
